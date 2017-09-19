19a296406bbcc4b9b11e9d62cca2af518737515e
temp <temp>
2017-09-19 13:37:39 -0400

    Add project files.

---
+++ TaurusCard/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace TaurusCard
+{
+    partial class taurusCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // taurusCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::TaurusCard.Properties.Resources.taurusBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "taurusCardForm";
+            this.Text = "Taurus Greeting Card";
+            this.Shown += new System.EventHandler(this.taurusCardForm_Shown);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ TaurusCard/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace TaurusCard
+{
+    public partial class taurusCardForm : Form
+    {
+        public taurusCardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void taurusCardForm_Shown(object sender, EventArgs e)
+        {
+            // Graphics set up and title screen
+            Graphics cardGraphics = this.CreateGraphics();
+            Pen linePen = new Pen(Color.White, 2);
+            SolidBrush starBrush = new SolidBrush(Color.White);
+            SolidBrush textBrush = new SolidBrush(Color.Brown);
+            Font titleFont = new Font("Arial", 16, FontStyle.Bold);
+
+            //title
+            cardGraphics.DrawString("Taurus", titleFont, textBrush, 15, 110);
+            cardGraphics.DrawString("Greeting", titleFont, textBrush, 15, 140);
+            cardGraphics.DrawString("Card", titleFont, textBrush, 15, 170);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // cardGraphics.Clear(Color.Black);
+        }
+        
+    }
+}
